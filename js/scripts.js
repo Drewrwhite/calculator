602264cc9931f7f6fcf443350b455fae14bdb67c
Drew White <d.white0002@gmail.com>
2022-10-17 15:45:20 -0700

    fix error with prompts

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,14 @@
 function add(number1, number2) {
   return number1 + number2;
 }
-function subtract(number1, number2) {
-  return number1 - number2;
+function subtract(number3, number4) {
+  return number3 - number4;
 }
-function multiply(number1, number2) {
-  return number1 * number2;
+function multiply(number5, number6) {
+  return number5 * number6;
 }
-function divide(number1, number2) {
-  return number1 / number2;
+function divide(number7, number8) {
+  return number7 / number8;
 }
 
 
@@ -18,3 +18,18 @@ const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
 window.alert(add(number1, number2));
+
+const number3 = parseInt(prompt("Enter a number:"));
+const number4 = parseInt(prompt("Enter another number:"));
+
+window.alert(subtract(number3, number4));
+
+const number5 = parseInt(prompt("Enter a number:"));
+const number6 = parseInt(prompt("Enter another number:"));
+
+window.alert(multiply(number5, number6));
+
+const number7 = parseInt(prompt("Enter a number:"));
+const number8 = parseInt(prompt("Enter another number:"));
+
+window.alert(divide(number7, number8));
